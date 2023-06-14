02800c740d1aacb095a21292f0821fc56d073380
CommanderGeek <thespidermann10@gmail.com>
2023-06-14 08:44:06 +0200

    added the game over screen!

--- Shouting Bird/Assets/LogicScript.cs
+++ Shouting Bird/Assets/LogicScript.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
 
     public int playerScore;
     public Text ScoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -16,6 +18,13 @@ public class LogicScript : MonoBehaviour
         ScoreText.text = playerScore.ToString();
     }
 
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver(){
+        gameOverScreen.SetActive(true);
+    }
     // Start is called before the first frame update
     void Start()
     {
