48aa2cc968b2e5b02251212b9d96a5b020c13128
CommanderGeek <thespidermann10@gmail.com>
2023-06-19 15:41:35 +0200

    Update PipeSpawner.cs

--- Shouting Bird/Assets/PipeSpawner.cs
+++ Shouting Bird/Assets/PipeSpawner.cs
@@ -34,7 +34,7 @@ public class PipeSpawner : MonoBehaviour
     {
         float lowestPoint = transform.position.y - heightOffset;
         float highestPoint = transform.position.y + heightOffset;
-        float spawnX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 10)).x;
+        float spawnX = Camera.main.ViewportToWorldPoint(new Vector3(1.1f, 0.5f, 10)).x;
         Instantiate(pipe, new Vector3(spawnX, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
